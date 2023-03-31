95d75900e267632c0a2d9ebcc46a2ceca1c16a53
Yunhao Tian <t123yh@outlook.com>
2023-03-31 13:35:56 +0800

    Move packed extension to nginx

--- pack.sh
+++ pack.sh
@@ -2,4 +2,5 @@
 set -e
 google-chrome --pack-extension=build/chrome-mv3-prod --pack-extension-key=fydeos-rime-extention.pem
 mkdir -p ../rel/
-mv build/chrome-mv3-prod.crx ../rel/latest.crx
+chmod 644 build/chrome-mv3-prod.crx
+sudo mv build/chrome-mv3-prod.crx /var/www/html/chinese-rime2.crx
