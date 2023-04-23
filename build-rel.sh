74b4d5f76f3b157200a6f95fbc70d34e9b14611b
Yunhao Tian <t123yh@outlook.com>
2023-04-23 12:23:53 +0800

    build-rel: use built-in input view

--- build-rel.sh
+++ build-rel.sh
@@ -3,7 +3,7 @@ set -e
 BUILD_DIR=build/chrome-mv3-prod
 rm -r $BUILD_DIR
 pnpm run build
-rsync -r --exclude .git ../inputview $BUILD_DIR
+rsync -r inputview $BUILD_DIR
 google-chrome --pack-extension=$BUILD_DIR --pack-extension-key=fydeos-rime-extention.pem
 mkdir -p ../rel/
 chmod 644 $BUILD_DIR.crx
